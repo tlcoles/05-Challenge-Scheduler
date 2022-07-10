9b6141a41124e141992b17c9ce5ae6065ada9e3f
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-07-10 12:05:00 +0200

    Change function for color to use css selectors, correct setInterval

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,35 @@
-//* Use Luxon to insert current day and date in header section
-setInterval(() => {
-  var DateTime = luxon.DateTime;
-  const dt = DateTime.now()
-  $("#currentDay").html(dt.toLocaleString(DateTime.DATE_HUGE));
-  $("#currentHour").html("Current time: " + DateTime.local().hour + ":" + DateTime.local().minute);
-}, 1000);
+const DateTime = luxon.DateTime; // call luxon for time
+const dt = DateTime.now(); // set current date
 
+//* Use Luxon to keep current day and time in header section
+setInterval(highlightCurrentHour, 10000); // call for the current hour every second
 
-//? Key requirements
-//! Color shift according to past/present/future time
-//! Local storage (which was covered in Wk 4 ex 21 and 22)
+function highlightCurrentHour() {
+  let currentHour = DateTime.local().hour; // set the current hour
+  $("#current-day").html(dt.toLocaleString(DateTime.DATE_HUGE)); // show the current date
+  $("body").addClass(`now-${currentHour}`); // add the current hour as a class to the body element to use css selectors for color change
+   }
+highlightCurrentHour(); // invoke immediately so there is no visual delay 
 
-//? Selectors
-//* $("#idname").action()
-//* $(".classname").action()
-//* $("elementname").action()
-//* $("div").css("background-color", "orange")
-//* $("#idname, .classname").css("background-color", "blue")
+const submitTask = function(event) {
+  event.preventDefault();
+  let tasksArray = []
+  const taskID = $("task-entry").attr("id")
+  console.log(taskID);
+  const task = $(".task-entry").val();
+  console.log(task);
+  localStorage.setItem
+  JSON.parse(localStorage.getItem("IDs"))
+}
+
+function saveEntry() {
+  // var appointment = $("textarea#appointment-entry").val();
+  console.log(this.id)
+  console.log(appointment)
+  var div2 = $(".row div:nth-child(2)")
+  div2.css("background-color", "red")
+}
+
 
 // TODO localStorage
 // the preferred approach is
@@ -27,24 +40,3 @@ setInterval(() => {
 //* Listen for click on save button
 //* Get the id
 //* For the sibling of that id, color the element red
-
-// $("[role='button']").click(function(){
-//   var id = $([role='button']).attr("id");
-//   alert(id);
-  // var btn = $("[id='this.id'], [role='button']");
-  // btn.siblings(".appointment-entry").css("background-color", "red")
-  // console.log(btn)
-  // console.log(btn.siblings)
-  // console.log(this.id)
-  // console.log(appointment)
-  // var div2 = $(".row div:nth-child(2)")
-  // div2.css("background-color", "red")
-// })
-
-function saveEntry() {
-  // var appointment = $("textarea#appointment-entry").val();
-  console.log(this.id)
-  console.log(appointment)
-  var div2 = $(".row div:nth-child(2)")
-  div2.css("background-color", "red")
-}
