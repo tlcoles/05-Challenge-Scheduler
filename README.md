9dd29cfdd1cde21e9458552755774e3652d20783
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-07-10 15:55:36 +0200

    Clean up files and update readme

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # Third-Party APIs Challenge: Work Day Scheduler
 An assignment of the ESMT Coding Boot Camp. Apply your skills to create a simple calendar application. Use HTML, CSS, and JQuery as well as third-party APIs.
 ## Snapshot
+![Screenshot of scheduler](./assets/images/221006-screenshot-Work-Day-Scheduler.png)
 
 ## Special challenges
+* Using jQuery
+* New APIs: Google Fonts, Luxon (!Momentjs) and FontAwesome
+* Improving design with Bootstrap 5
+* Using nth child selector
+* localStorage.setItem and localStorage.getItem
+* jQuery .each() vs JavaScript for() loop
+
 
 ## Gratitude
 * Andrew Evans of Rhythm and Binary for [Using Luxon for JavaScript Dates and Times](https://youtu.be/zIRSdTKyPS4)
+* Morgan Roderick for general support and redirection from JavaaScript to CSS for time highlighting
+* ESMT Coding Boot Camp teachers and tutors for instruction, resource suggestions, and support
+* Study buddies Henriette and Tornike for burning the midnight oil with me
 ## Contact
 Tammi L. Coles  
 URL of deployed application: https://tlcoles.github.io/05-Challenge-Scheduler/  
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,31 @@
-const DateTime = luxon.DateTime; // call luxon for time
-const dt = DateTime.now(); // set current date
+//* Use Luxon API to keep current day and time 
+const DateTime = luxon.DateTime; // Call Luxon API for time
+const dt = DateTime.now(); // Set current date
 
-//* Use Luxon to keep current day and time in header section
-setInterval(highlightCurrentHour, 10000); // call for the current hour every second
-
+// Use "now-" and currentHour value as a class for the body element to use css selectors for color
 function highlightCurrentHour() {
   let currentHour = DateTime.local().hour; // set the current hour
   $("#current-day").html(dt.toLocaleString(DateTime.DATE_HUGE)); // show the current date
-  $("body").addClass(`now-${currentHour}`); // add the current hour as a class to the body element to use css selectors for color
+  $("body").addClass(`now-${currentHour}`); 
    }
+
+setInterval(highlightCurrentHour, 10000); // call for the current hour every second
 
 //* Use .on as event listener for submit type
 $("form").on("submit", function (e) {
   e.preventDefault();
 
-// Loop through textareas to place content in localStorage
+//* Loop through textareas to place content in localStorage
   $("textarea").each(function (indexInArray, element) { 
-   localStorage.setItem(element.id, element.value)
+   localStorage.setItem(element.id, element.value);
 });
 });
 
-// Use localStorage to show values on page
+//* Use localStorage to show values on page
 $("document").ready(function (e) {
   highlightCurrentHour(); // invoke immediately so there is no visual delay 
   $("textarea").each(function (indexInArray, element) {
     let value = localStorage.getItem(element.id);
-    element.value = value
+    element.value = value;
   });
 })
-
-
-// TODO Understand localStorage and jQuery .each and .ready
-// localStorage.setItem(key,value); to set
-// localStorage.getItem(key); to get
